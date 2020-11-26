eb994345ce3793c98f2406ad90f103e6a8991df8
Jason <165161118@qq.com>
2020-11-26 22:22:40 +0800

    second

--- heima/duboo/dubooProvider/src/main/java/com/heima/service/Impl/HelloImpl.java
+++ heima/duboo/dubooProvider/src/main/java/com/heima/service/Impl/HelloImpl.java
@@ -1,7 +1,13 @@
 package com.heima.service.Impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.heima.service.Hello;
 
 @Service
 public class HelloImpl implements Hello {
+
+    @Override
+    public String sayHello(String name) {
+        return "hello"+ name;
+    }
 }
